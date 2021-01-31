906afa8b2bd735cc20fefaac8a2c3dd38fd98d4e
Cindy Le <cindyledev@gmail.com>
2021-01-31 17:46:11 -0500

    add dependencies and create s3 bucket for images

--- lib/aws-devhr-stack.ts
+++ lib/aws-devhr-stack.ts
@@ -1,9 +1,19 @@
 import * as cdk from '@aws-cdk/core';
+import s3 = require('@aws-cdk/aws-s3');
+import lambda = require('@aws-cdk/aws-lambda');
+import dynamodb = require('@aws-cdk/aws-dynamodb');
+import iam = require('@aws-cdk/aws-iam');
+import event_sources = require('@aws-cdk/aws-lambda-event-sources');
+import { Duration } from '@aws-cdk/core';
+
+const imageBucketName = 'cdk-rekn-imagebucket'
 
 export class AwsDevhrStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Image Bucket
+    const imageBucket = new s3.Bucket(this, imageBucketName)
+    new cdk.CfnOutput(this, 'imageBucket', { value: imageBucket.bucketName });
   }
 }
