ea7382dfb28080e6ab824fe8a4952de87fe1dc93
Cindy Le <cindyledev@gmail.com>
2021-01-31 18:48:34 -0500

    created lambda function and add rekognition assets

--- lib/aws-devhr-stack.ts
+++ lib/aws-devhr-stack.ts
@@ -12,15 +12,39 @@ export class AwsDevhrStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Image Bucket
+    // Create S3 Image Bucket
     const imageBucket = new s3.Bucket(this, imageBucketName)
     new cdk.CfnOutput(this, 'imageBucket', { value: imageBucket.bucketName });
 
-    // DynamoDB
+    // Create DynamoDB table for storing image labels
     const table = new dynamodb.Table(this, 'ImageLabels', {
       partitionKey: { name: 'image', type: dynamodb.AttributeType.STRING },
       removalPolicy: cdk.RemovalPolicy.DESTROY
     });
     new cdk.CfnOutput(this, 'ddbTable', { value: table.tableName });
+
+    // Create Lambda function
+    const rekFn = new lambda.Function(this, 'rekognitionFunction', {
+      code: lambda.Code.fromAsset('rekognitionlambda'),
+      runtime: lambda.Runtime.PYTHON_3_7,
+      handler: 'index.handler',
+      timeout: Duration.seconds(30),
+      memorySize: 1024,
+      environment: {
+        "TABLE": table.tableName,
+        "BUCKET": imageBucket.bucketName,
+      },
+    });
+    rekFn.addEventSource(new event_sources.S3EventSource(imageBucket, { events: [ s3.EventType.OBJECT_CREATED ] }));
+    imageBucket.grantRead(rekFn);
+    table.grantWriteData(rekFn);
+
+    rekFn.addToRolePolicy(new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: ['rekognition:DetectLabels'],
+      resources: ['*']
+    }));
+
+
   }
 }
